7dd79412d4a454765c248a40eae50089f0db3ca0
omkaruttirnaservice <omkaruttirnaservice@gmail.com>
2025-09-02 10:44:53 +0530

    ref

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
         apps: [
                 {
-                        name: "vaibhav-thakre.mns.com",
+                        name: "shailesh-dhage.mns.com",
                         script: "server.js",
                         instances: 1,
                         autorestart: true,
